f079d8aee9c122e5e8cb15eaf007e11d4a9b7f7c
Farhan <mufr31236@gmail.com>
2024-09-04 03:10:49 +0700

    Fix seeder

--- cmd/seeder/seeder.go
+++ cmd/seeder/seeder.go
@@ -38,9 +38,10 @@ func main() {
 
 	for i := 0; i < 2; i++ {
 		hospitalId, err := hospitalService.AddHospital(entities.Hospital{
-			Name:          fakerObj.Company().Name(),
+			Name:          "Rumah Sakit " + fakerObj.Company().Name(),
 			Address:       fakerObj.Address().Address(),
 			ContactNumber: "+628-1234-4432-12",
+			Image:         "https://mysiloam-api.siloamhospitals.com/public-asset/website-cms/website-cms-16273629729867208.jpg",
 		})
 		if err != nil {
 			panic(err)
@@ -50,6 +51,7 @@ func main() {
 			doctorId, err := doctorService.AddDoctor(entities.Doctor{
 				Name:       fakerObj.Person().Name(),
 				HospitalId: hospitalId,
+				Image:      "https://img.freepik.com/free-photo/healthcare-workers-preventing-virus-quarantine-campaign-concept-cheerful-friendly-asian-female-physician-doctor-with-clipboard-daily-checkup-standing-white-background_1258-107867.jpg?t=st=1725206635~exp=1725210235~hmac=530ce81a8db49a367f1db2d4ba1478fe11615de60264e182d8f2d2060567eb90&w=996",
 			})
 			if err != nil {
 				panic(err)
@@ -63,9 +65,9 @@ func main() {
 					if err != nil {
 						panic(err)
 					}
-					hour = baseTime.Add(time.Duration(z*30) * time.Minute)
+					hour = hour.Add(time.Duration(30) * time.Minute)
 				}
-				hour = baseTime.Add(-time.Duration(335) * time.Minute)
+				hour = hour.Add(-time.Duration(150) * time.Minute)
 			}
 		}
 
@@ -73,22 +75,21 @@ func main() {
 			doctorId, err := doctorService.AddDoctor(entities.Doctor{
 				Name:       fakerObj.Person().Name(),
 				HospitalId: hospitalId,
+				Image:      "https://img.freepik.com/free-photo/healthcare-workers-preventing-virus-quarantine-campaign-concept-cheerful-friendly-asian-female-physician-doctor-with-clipboard-daily-checkup-standing-white-background_1258-107867.jpg?t=st=1725206635~exp=1725210235~hmac=530ce81a8db49a367f1db2d4ba1478fe11615de60264e182d8f2d2060567eb90&w=996",
 			})
 			if err != nil {
 				panic(err)
 			}
 			for k := 0; k < 5; k++ {
-				for z := 0; z < 5; z++ {
-					err = doctorService.AddDoctorOnDutySchedule(entities.DoctorOnDuty{
-						Day:          days[z],
-						StartHour:    "08:00",
-						EndHour:      "14:00",
-						PatientLimit: 20,
-						DoctorId:     doctorId,
-					})
-					if err != nil {
-						panic(err)
-					}
+				err = doctorService.AddDoctorOnDutySchedule(entities.DoctorOnDuty{
+					Day:          days[k],
+					StartHour:    "08:00",
+					EndHour:      "14:00",
+					PatientLimit: 20,
+					DoctorId:     doctorId,
+				})
+				if err != nil {
+					panic(err)
 				}
 
 			}
